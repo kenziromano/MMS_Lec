dc6c540bcf9eb8ca278649d0bb9ac8302a56b245
Kenluu1 <kenneth.loe@binus.ac.id>
2025-12-15 12:40:16 +0700

    mms lec update

---
+++ LEC_MMS/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  LEC_MMS
+//
+//  Created by eslilinnn on 14/12/25.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
